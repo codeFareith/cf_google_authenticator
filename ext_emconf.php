24ffdf73dc91bbae704f251d3e94832a69f7d53f
Robin von den Bergen <robinvonberg@gmx.de>
2018-07-16 19:30:32 +0200

    v1.0.3
    - a lot of refactoring and -structuring

--- ext_emconf.php
+++ ext_emconf.php
@@ -27,7 +27,7 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => '',
 
     'state' => 'stable',
-    'version' => '1.0.2',
+    'version' => '1.0.3',
 
     'uploadFolders' => false,
     'createDirs' => '',
