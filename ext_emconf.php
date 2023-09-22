5f494fd364975fb4637d667cfa2b32e6b5c7ed2e
Xavier Perseguers <xavier@causal.ch>
2023-09-22 18:48:19 +0200

    [TASK] Allow stable version of chillerlan/php-qrcode

--- ext_emconf.php
+++ ext_emconf.php
@@ -23,7 +23,7 @@ $EM_CONF[$_EXTKEY] = [
     'clearCacheOnLoad' => true,
     'constraints' => [
         'depends' => [
-            'php' => '7.4-',
+            'php' => '7.4.0-8.2.99',
             'typo3' => '11.5.0-11.5.99',
             'felogin' => '11.5.0-11.5.99',
         ],
